be260afd032741da42fbb12ed961e5f13b05cfed
Nick Furlo <9084612+furlopunk@users.noreply.github.com>
2018-04-03 17:59:56 -0400

    Added javadoc comments

--- app/src/main/java/com/example/user/oumobileapp/Home.java
+++ app/src/main/java/com/example/user/oumobileapp/Home.java
@@ -11,59 +11,77 @@ import android.widget.Button;
  * Created by Jonathan Lacoursiere on 3/6/2018.
  */
 
+
 public class Home extends Activity {
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+
+    /**
+     * @param savedInstanceState
+     */
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
 
         //The buttons to get to the their respective use cases
-        Button goFinAid = (Button)findViewById(R.id.goFinAidBtn);
-        goFinAid.setOnClickListener(new View.OnClickListener(){
+        Button goFinAid = (Button) findViewById(R.id.goFinAidBtn);
+        goFinAid.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
 
-                Intent goFin = new Intent(getApplicationContext(),finAid.class);
+                Intent goFin = new Intent(getApplicationContext(), finAid.class);
                 startActivity(goFin);
             }
         });
 
-        Button goAdv = (Button)findViewById(R.id.goAdvisingBtn);
-        goAdv.setOnClickListener(new View.OnClickListener(){
+        Button goAdv = (Button) findViewById(R.id.goAdvisingBtn);
+        goAdv.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
 
-                Intent goAdv = new Intent(getApplicationContext(),advSelect.class);
+                Intent goAdv = new Intent(getApplicationContext(), advSelect.class);
                 startActivity(goAdv);
             }
         });
-
     }
-public void openActivity(View view){
+
+    /**
+     * Open grades activity upon clicking grades button
+     *
+     * @param view current view
+     */
+    public void openActivity(View view) {
         String btnText;
         btnText = ((Button) view).getText().toString();
-        if(btnText.equals("Grades")) {
+        if (btnText.equals("Grades")) {
             Intent i = new Intent(this, Grades.class);
             startActivity(i);
         }
     }
 
-    public void openActivityBus(View view){
-            Intent i = new Intent(this, bus.class);
-            startActivity(i);
+    /**
+     * Open bus activity upon clicking bus tracker button
+     *
+     * @param view current view
+     */
+    public void openActivityBus(View view) {
+        Intent i = new Intent(this, bus.class);
+        startActivity(i);
     }
 
+    /**
+     * queried PHP server for grizz ID of current authenticated user.
+     *
+     * @param view current view
+     */
+    public void getGrizzID(View view) {
+        String username = "tester@tester.com";
+        String password = "shouldnt_need";
+        String type = "login";
 
+        getGrizz getGrizz = new getGrizz(this);
+        getGrizz.execute(type, username, password);
 
-public void   getGrizzID(View view){
-    String username = "tester@tester.com";
-    String password = "shouldnt_need";
-    String type = "login";
-
-    getGrizz getGrizz = new getGrizz(this);
-    getGrizz.execute(type, username, password);
-
-}
+    }
 }
