a038aea51c6e373af5d70b3a9c92afb3b6081be6
Anton <ajcataldo15@gmail.com>
2018-04-10 23:57:58 -0400

    added advisor info prepped fin aid for database  still needs to be complete

--- app/src/main/java/com/example/user/oumobileapp/AdvisorInfo.java
+++ app/src/main/java/com/example/user/oumobileapp/AdvisorInfo.java
@@ -2,6 +2,8 @@ package com.example.user.oumobileapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AdvisorInfo extends AppCompatActivity {
 
@@ -14,5 +16,31 @@ public class AdvisorInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advisor_info);
+
+        TextView name2 = (TextView)findViewById(R.id.nameTwoTextvie) ;
+        TextView name3 = (TextView)findViewById(R.id.adName3) ;
+        TextView name4 = (TextView)findViewById(R.id.adname4) ;
+
+        TextView num2 = (TextView)findViewById(R.id.adNum2) ;
+        TextView num3 = (TextView)findViewById(R.id.adnum3) ;
+        TextView num4 = (TextView)findViewById(R.id.adnumfour) ;
+
+        TextView email2 = (TextView)findViewById(R.id.advisor2) ;
+        TextView email3 = (TextView)findViewById(R.id.advisor3) ;
+        TextView email4 = (TextView)findViewById(R.id.textView18) ;
+
+
+        name2.setText("Tim Johnson");
+        name3.setText("Kim Orlando");
+        name4.setText("Nate Edwards");
+
+        num2.setText("458-562-4881");
+        num3.setText("496-023-0004");
+        num4.setText("689-413-2952");
+
+        email2.setText("tj@oakland.edu");
+        email3.setText("klando@oakland.edu");
+        email4.setText("Nedwards@oakland.edu");
     }
+
 }
--- app/src/main/java/com/example/user/oumobileapp/finAid.java
+++ app/src/main/java/com/example/user/oumobileapp/finAid.java
@@ -2,6 +2,7 @@ package com.example.user.oumobileapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class finAid extends AppCompatActivity {
 
@@ -14,5 +15,17 @@ public class finAid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin_aid);
+
+        TextView name1 = (TextView) findViewById(R.id.aName1);
+        TextView name2 = (TextView) findViewById(R.id.aName2);
+
+        TextView amount1 = (TextView) findViewById(R.id.aAmount1);
+        TextView amount2 = (TextView) findViewById(R.id.aAmount1);
+/*
+        name1.setText();
+        name2.setText();
+
+        amount1.setText();
+        amount2.setText(); */
     }
 }
