06218a193436dd125781dfe37e8b5c27d6126a86
Anton <ajcataldo15@gmail.com>
2018-04-16 13:47:44 -0400

    DB

--- app/src/main/java/com/example/user/oumobileapp/finAid.java
+++ app/src/main/java/com/example/user/oumobileapp/finAid.java
@@ -4,14 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class finAid extends AppCompatActivity {
 
     /**
      * Initializes activity.
      *
-     * @param savedInstanceState current state to be saved and passed
+
      */
-    @Override
+
+    protected String doInBackFA(){
+        String finAidURL = "http://www.secs.oakland.edu/~ksmith/aidInfo.php";
+
+        try {
+            URL url = new URL(finAidURL);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            InputStream inputStream = httpURLConnection.getInputStream();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+            String result = "";
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                result += line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+            return result;
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin_aid);
@@ -21,11 +64,15 @@ public class finAid extends AppCompatActivity {
 
         TextView amount1 = (TextView) findViewById(R.id.aAmount1);
         TextView amount2 = (TextView) findViewById(R.id.aAmount1);
-/*
-        name1.setText();
-        name2.setText();
 
-        amount1.setText();
-        amount2.setText(); */
+        String a = doInBackFA();
+
+
+        name1.setText("Deans Award");
+
+        name2.setText("Geographical Award");
+
+        amount1.setText("5000");
+        amount2.setText("2000");
     }
 }
